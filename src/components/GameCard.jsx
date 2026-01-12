e8715530749a6c0a96f8945fec0fe9689e3918bc
Gonzalo Nuñez <gonzalonunez@gmail.com>
2026-01-12 20:03:20 -0300

    soluciono import de css

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,32 +1,30 @@
 import { useNavigate } from "react-router-dom";
-import "./GameCard.css";
+import "./gameCard.css";
 
 export default function GameCard({ game }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    if (!game) return null;
+  if (!game) return null;
 
-    const handleClick = () => {
-        navigate("*"); // va al Error404
-    };
+  const handleClick = () => {
+    navigate("*"); // va al Error404
+  };
 
-    return (
-        <article className="gameCard" onClick={handleClick}>
-            <div className="gameImage">
-                {game.imagen ? (
-                    <img src={game.imagen} alt={game.nombre} />
-                ) : (
-                    <div className="noImage">Sin imagen</div>
-                )}
-            </div>
+  return (
+    <article className="gameCard" onClick={handleClick}>
+      <div className="gameImage">
+        {game.imagen ? (
+          <img src={game.imagen} alt={game.nombre} />
+        ) : (
+          <div className="noImage">Sin imagen</div>
+        )}
+      </div>
 
-            <div className="gameInfo">
-                <span className="category">{game.categoria}</span>
-                <h3 className="title">{game.nombre}</h3>
-                <p className="description">{game.descripcion}</p>
-            </div>
-        </article>
-    );
+      <div className="gameInfo">
+        <span className="category">{game.categoria}</span>
+        <h3 className="title">{game.nombre}</h3>
+        <p className="description">{game.descripcion}</p>
+      </div>
+    </article>
+  );
 }
-
-
