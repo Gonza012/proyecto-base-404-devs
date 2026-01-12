c82cbd62a1ee6edbb1a3fd0cddac5c547535cac7
Gonzalo Nuñez <gonzalonunez@gmail.com>
2026-01-12 19:43:47 -0300

    fix import loginCss

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 
 function Login() {
   const [errorLogin, setErrorLogin] = useState("");
