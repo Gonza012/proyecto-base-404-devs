af97dd4b7c3db36c37f50bc6f07f4db5ad53c0a4
Gonzalo Nuñez <gonzalonunez@gmail.com>
2026-01-12 19:41:50 -0300

    modifico import de about us css

--- src/pages/aboutus/aboutus.jsx
+++ src/pages/aboutus/aboutus.jsx
@@ -1,10 +1,10 @@
-import "./nosotros.css";
+import "./aboutUs.css";
 import "./member-card.css";
 import MemberCard from "./member-card";
-import A from "../../assets/Agustin-Imagen.png"
-import Nico from "../../assets/Nico-Imagen.png"
-import Gonzalo from "../../assets/Gonzalo-Imagen2.png"
-import Ezequiel from "../../assets/Ezequiel-Imagen.jpg"
+import A from "../../assets/Agustin-Imagen.png";
+import Nico from "../../assets/Nico-Imagen.png";
+import Gonzalo from "../../assets/Gonzalo-Imagen2.png";
+import Ezequiel from "../../assets/Ezequiel-Imagen.jpg";
 
 function Nosotros() {
   return (
@@ -12,27 +12,18 @@ function Nosotros() {
       <h1 className="titulo-equipo">Nuestro Equipo</h1>
 
       <div className="team-container">
-        <MemberCard 
-          nombre = "Ezequiel"
-          imagen = {Ezequiel}
-        />
-        <MemberCard 
-          nombre = "Gonzalo"
-          imagen = {Gonzalo}
-        />
-        <MemberCard 
-          nombre = "Nico"
-          imagen = {Nico}
-        />
-        <MemberCard 
-          nombre = "Agustin"
-          imagen = {A}
-        />
+        <MemberCard nombre="Ezequiel" imagen={Ezequiel} />
+        <MemberCard nombre="Gonzalo" imagen={Gonzalo} />
+        <MemberCard nombre="Nico" imagen={Nico} />
+        <MemberCard nombre="Agustin" imagen={A} />
       </div>
-      <p>Somos un equipo apasionado por los videojuegos.
-Trabajamos para ofrecerte los mejores títulos y experiencias, desde clásicos hasta los últimos lanzamientos.</p>
+      <p>
+        Somos un equipo apasionado por los videojuegos. Trabajamos para
+        ofrecerte los mejores títulos y experiencias, desde clásicos hasta los
+        últimos lanzamientos.
+      </p>
     </section>
-  )
+  );
 }
 
-export default Nosotros
+export default Nosotros;
